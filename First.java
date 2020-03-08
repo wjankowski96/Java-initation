b49eb9a3ef6c23b96fd49aae94650cf378b536bb
Wojciech Jankowski <wjankowski96@wp.pl>
2020-03-08 13:40:35 +0100

    Program pole koła liczący numerycznie i analitycznie

--- First.java
+++ First.java
@@ -1,20 +1,42 @@
 
+import java.util.Scanner;
+import java.util.Random;
 public class First {
     public static void main(String[] args) {
      
-        int[] tab;
-        tab = new int[5];
-        for (int i=0; i<5; i++)
-            {
-                tab[i]=i+1;
-            }
-        for (int i=0; i<5; i++)
-            {
-            System.out.println(tab[i]);
-            }
-        System.out.println("Martyna");
+     double pole, polekw, x, y; //pole koła, pole kwadratu, wsp x, wsp y
+    int r, p, k; // promien, liczba probek, k
 
-        System.out.println(tab.lenght);
+    Scanner in = new Scanner(System.in);
+    System.out.println("Podaj promień koła");
+    r = in.nextInt();
+
+    System.out.println("Promień koła: {0}"+ r);
+    System.out.println("Podaj liczbę probek");
+    p = in.nextInt();
+
+    System.out.println("Liczba próbek: {0}"+ p);
+
+    pole = Math.PI * r * r;
+    System.out.println("Pole koła obliczone analitycznie: " + pole);
+    
+    polekw = 4 * r * r;
+    Random a = new Random();
+    k = 0;
+    for (int i = 1; i < p + 1; i++)
+    {
+        x = a.nextDouble();
+        y = a.nextDouble();
+        if ((x * x + y * y) <= r * r)
+        {
+            k++;
+        }
+    }
+    pole = polekw * k / p;
+    System.out.println("Pole koła obliczone numerycznie: {0}"+ pole);
+
+
+        
        
     }
     
